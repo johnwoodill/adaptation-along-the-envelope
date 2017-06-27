889a1835f8241b13226baabcbb7af78a6b1f4e0d
john <johnwoodill@gmail.com>
2017-06-26 15:25:53 -1000

    check

--- cross_section_regression.R
+++ cross_section_regression.R
@@ -15,8 +15,12 @@ g_legend<-function(a.gplot){
 
 
 cropdat <- readRDS("data/full_ag_data.rds")
+
+# East of 100th meridian
 cropdat <- filter(cropdat, abs(long) <= 100)
-#cropdat <- filter(cropdat, year >= 1960 & year <= 2010)
+cropdat <- filter(cropdat, year >= 1960 & year <= 2010)
+
+# Crop rev
 cropdat$corn_rev <- (cropdat$corn_grain_p*cropdat$corn_rprice)/cropdat$corn_grain_a
 cropdat$cotton_rev <- (cropdat$cotton_p*cropdat$cotton_rprice)/cropdat$cotton_a
 cropdat$hay_rev <- (cropdat$hay_p*cropdat$hay_rprice)/cropdat$hay_a 
@@ -26,44 +30,84 @@ cropdat$soybean_rev <- (cropdat$soybean_p*cropdat$soybean_rprice)/cropdat$soybea
 cropdat$total_rev <- cropdat$corn_rev + cropdat$cotton_rev + cropdat$hay_rev + cropdat$wheat_rev + cropdat$soybean_rev
 cropdat$total_a <- cropdat$corn_grain_a + cropdat$cotton_a + cropdat$hay_a + cropdat$wheat_a + cropdat$soybean_a
 
+cropdat$dd8C_30C <-cropdat$dday8C - cropdat$dday30C
+
+# Remove inf to na
+is.na(cropdat) <- do.call(cbind, lapply(cropdat, is.infinite))
+
+#do.call(data.frame,lapply(cropdat, function(x) replace(x, is.infinite(x),NA)))
+
+# Get annual averages
 dat <- cropdat %>%
   group_by(year) %>%
   summarise(annual_corn_rev = mean(corn_rev, na.rm = TRUE),
+            annual_cotton_rev = mean(cotton_rev, na.rm = TRUE),
+            annual_hay_rev = mean(hay_rev, na.rm = TRUE),
+            annual_wheat_rev = mean(wheat_rev, na.rm = TRUE),
+            annual_soybean_rev = mean(soybean_rev, na.rm = TRUE),
          annual_tavg = mean(tavg, na.rm = TRUE),
-         annual_prec = mean(prec, na.rm = TRUE)) %>%  
+         annual_prec = mean(prec, na.rm = TRUE),
+         annual_dd8C_30C = mean(dd8C_30C, na.rm = TRUE),
+         annual_dday34C = mean(dday34C, na.rm = TRUE)) %>%  
     ungroup()
 
-########################
-# Producing lots of NA"s for some reason
-#
-#
-#
-#
+dat <- left_join(cropdat, dat, by = "year")
 
-# dat$dm_corn_rev <- log(1 + dat$corn_rev) - log(1 + dat$annual_corn_rev)
-# dat$dm_tavg <- dat$tavg - dat$annual_tavg  
-# dat$dm_prec <- dat$prec - dat$annual_prec
+# Demean each crop and temps
+dat$dm_corn_rev <- log(1 + dat$corn_rev) - log(1 + dat$annual_corn_rev)
+dat$dm_cotton_rev <- log(1 + dat$cotton_rev) - log(1 + dat$annual_cotton_rev)
+dat$dm_hay_rev <- log(1 + dat$hay_rev) - log(1 + dat$annual_hay_rev)
+dat$dm_wheat_rev <- log(1 + dat$wheat_rev) - log(1 + dat$annual_wheat_rev)
+dat$dm_soybean_rev <- log(1 + dat$soybean_rev) - log(1 + dat$annual_soybean_rev)
+dat$dm_tavg <- dat$tavg - dat$annual_tavg  
+dat$dm_prec <- dat$prec - dat$annual_prec
+dat$dm_dd8C_30C <- dat$dd8C_30C - dat$annual_dd8C_30C
+dat$dm_dd34C <- dat$dday34C - dat$annual_dday34C
 
-# group_by(fips, year) %>% 
-#   mutate(dm_corn_rev = log(1 + corn_rev) - log(1 + annual_corn_rev),
-#             dm_tavg = tavg - annual_tavg,
-#             dm_prec = prec - annual_prec) %>%  
-#   ungroup() %>% 
-
+# Aggregate to county level
 dat <- dat %>%
-  group_by(fips) %>% 
-  summarise(corn_rev = mean(log(1 + corn_rev) - log(1 + annual_corn_rev), na.rm = TRUE))
+   group_by(fips) %>% 
+   summarise(dm_corn_rev = mean(dm_corn_rev, na.rm = TRUE),
+             dm_cotton_rev = mean(dm_cotton_rev, na.rm = TRUE),
+             dm_hay_rev = mean(dm_hay_rev, na.rm = TRUE),
+             dm_wheat_rev = mean(dm_wheat_rev, na.rm = TRUE),
+             dm_soybean_rev = mean(dm_soybean_rev, na.rm = TRUE),
+             dm_tavg = mean(dm_tavg, na.rm = TRUE),
+             dm_prec = mean(dm_prec, na.rm = TRUE),
+             dm_dd8C_30C = mean(dm_dd8C_30C, na.rm = TRUE),
+             dm_dd34C = mean(dm_dd34C, na.rm = TRUE))
 
-            tavg = mean(dm_tavg, na.rm = TRUE),
-            prec = mean(dm_prec, na.rm = TRUE))
 
-dat[is.infinite(dat$corn_rev),] <- NA
-dat <- filter(dat, !is.na(corn_rev))
-dat$tavgsq <- dat$tavg^2
-dat$precsq <- dat$prec^2
+dat[is.infinite(dat$dm_corn_rev),] <- NA
+#dat <- filter(dat, !is.na(dm_corn_rev))
+dat$dm_tavgsq <- dat$dm_tavg^2
+dat$dm_precsq <- dat$dm_prec^2
 
-mod <- lm(corn_rev ~ tavg + tavgsq + precsq + prec, data = dat)
-mod
+# Corn
+mod1 <- lm(dm_corn_rev ~ dm_tavg + dm_tavgsq + dm_prec + dm_precsq, data = dat)
+summary(mod1)
+
+mod1 <- lm(dm_corn_rev ~ dm_dd8C_30C + dm_dd34C + dm_prec + dm_precsq, data = dat)
+summary(mod1)
+
+# Cotton
+mod2 <- lm(dm_cotton_rev ~ dm_tavg + dm_tavgsq + dm_prec + dm_precsq, data = dat)
+summary(mod2)
+
+# Hay
+mod3 <- lm(dm_hay_rev ~ dm_tavg + dm_tavgsq + dm_prec + dm_precsq, data = dat)
+summary(mod3)
+
+# Wheat
+mod4 <- lm(dm_wheat_rev ~ dm_tavg + dm_tavgsq + dm_prec + dm_precsq, data = dat)
+summary(mod4)
+
+# Soybean
+mod5 <- lm(dm_soybean_rev ~ dm_tavg + dm_tavgsq + dm_prec + dm_precsq, data = dat)
+summary(mod5)
+
+
+#######################################
 
 newdat <- cropdat %>% 
   group_by(fips) %>% 
--- panel_regression.R
+++ panel_regression.R
@@ -17,7 +17,7 @@ g_legend<-function(a.gplot){
 cropdat <- readRDS("data/full_ag_data.rds")
 cropdat <- filter(cropdat, abs(long) <= 100)
 cropdat <- filter(cropdat, year >= 1960 & year <= 2010)
-cropdat$corn_rev <- (cropdat$corn_grain_p*cropdat$corn_rprice)
+cropdat$corn_rev <- (cropdat$corn_grain_p*cropdat$corn_rprice)/cropdat$corn_grain_a
 cropdat$cotton_rev <- (cropdat$cotton_p*cropdat$cotton_rprice)
 cropdat$hay_rev <- (cropdat$hay_p*cropdat$hay_rprice)
 cropdat$wheat_rev <- (cropdat$wheat_p*cropdat$wheat_rprice)
@@ -33,5 +33,6 @@ cropdat$tavgsq <- cropdat$tavg^2
 cropdat$ffips <- as.factor(cropdat$fips)
 cropdat$fstate <- as.factor(cropdat$state)
 
-l6 <- lm(log(1 + corn_rev) ~ ffips + tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, weights = cropdat$corn_grain_a)
+l6 <- lm(log(1 + corn_rev) ~ ffips + tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat)
 #l6s <- lm(log(1 + corn_rev) ~ fstate + tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, weights = cropdat$corn_grain_a)
+l6
