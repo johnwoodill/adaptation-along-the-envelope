d30993341efaae2201917ee6cc7c31772db98272
john <johnwoodill@gmail.com>
2017-06-28 16:31:40 -1000

    working on panel data

--- panel_regression.R
+++ panel_regression.R
@@ -5,28 +5,12 @@ library(rms)
 library(cowplot)
 library(plm)
 
-# Function to extract legend from ggplot object
-g_legend<-function(a.gplot){
-    tmp <- ggplot_gtable(ggplot_build(a.gplot))
-    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-    legend <- tmp$grobs[[leg]]
-    legend
-}
-
-
 cropdat <- readRDS("data/full_ag_data.rds")
 cropdat <- filter(cropdat, abs(long) <= 100)
 cropdat <- filter(cropdat, year >= 1960 & year <= 2010)
-cropdat$corn_rev <- (cropdat$corn_grain_p*cropdat$corn_rprice)/cropdat$corn_grain_a
-cropdat$cotton_rev <- (cropdat$cotton_p*cropdat$cotton_rprice)/cropdat$cotton_a
-cropdat$hay_rev <- (cropdat$hay_p*cropdat$hay_rprice)/cropdat$hay_a
-cropdat$wheat_rev <- (cropdat$wheat_p*cropdat$wheat_rprice)/cropdat$wheat_a
-cropdat$soybean_rev <- (cropdat$soybean_p*cropdat$soybean_rprice)/cropdat$soybean_a
+#cropdat <- filter(cropdat, fips == 18175)
 
-cropdat$total_rev <- cropdat$corn_rev + cropdat$cotton_rev + cropdat$hay_rev + cropdat$wheat_rev + cropdat$soybean_rev
-cropdat$total_a <- cropdat$corn_grain_a + cropdat$cotton_a + cropdat$hay_a + cropdat$wheat_a + cropdat$soybean_a
 
-is.na(cropdat) <- do.call(cbind, lapply(cropdat, is.infinite))
 
 cropdat$trend <- cropdat$year - 1959
 cropdat$trendsq <- cropdat$trend^2
@@ -34,21 +18,38 @@ cropdat$precsq <- cropdat$prec^2
 cropdat$tavgsq <- cropdat$tavg^2
 cropdat$ffips <- as.factor(cropdat$fips)
 cropdat$fstate <- as.factor(cropdat$state)
+cropdat$dday8C_32C <- cropdat$dday8C - cropdat$dday32C
+cropdat$dday8C_32C_sq <- cropdat$dday8C_32C^2
+cropdat$dday34C_sqrt <- sqrt(cropdat$dday32C)
 
-#mod1 <- plm(log(1 + corn_rev) ~ tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, index = "ffips", weight = cropdat$corn_grain_a)
-mod1 <- plm(corn_rev ~ tavg + prec, data = cropdat, index = c("ffips"))
+dat <- cropdat
+dbdist <- datadist(dat)
+options("datadist" = "dbdist")
+
+#mod1 <- ols(log(corn_yield) ~ statefe + corn_dday10w + corn_dday29w,
+#            data=gp, x = TRUE, y = TRUE)
+
+mod1 <- plm(log(1 + corn_rrev) ~ dday8C_32C + dday8C_32C_sq + 
+             dday34C_sqrt + trend + trendsq + prec + precsq, data = dat, index = "state")
+mod1
 summary(mod1)
 test <- mod1$model
+plot(mod1)
 
-mod2 <- plm(log(1 + cotton_rev) ~ tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, index = c("ffips"))
+mod2 <- plm(log(1 + cotton_rrev) ~ dday8C_32C + dday8C_32C_sq + 
+             dday34C_sqrt + trend + trendsq + prec + precsq, data = dat, index = c("ffips"))
 summary(mod2)
 
-mod3 <- plm(log(1 + hay_rev) ~ tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, index = "ffips")
+mod3 <- plm(log(1 + hay_rrev) ~ dday8C_32C + dday8C_32C_sq + 
+             dday34C_sqrt + trend + trendsq + prec + precsq, data = dat, index = "ffips")
 summary(mod3)
 
-mod4 <- plm(log(1 + wheat_rev) ~ tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, index = "ffips")
+mod4 <- plm(log(1 + wheat_rrev) ~ ~ dday8C_32C + dday8C_32C_sq + 
+             dday34C_sqrt + trend + trendsq + prec + precsq, data = dat, index = "ffips")
 summary(mod4)
 
-mod5 <- plm(log(1 + soybean_rev) ~ tavg + tavgsq + trend + trendsq + prec + precsq, data = cropdat, index = "ffips")
+mod5 <- plm(log(1 + soybean_rrev) ~ ~ dday8C_32C + dday8C_32C_sq + 
+             dday34C_sqrt + trend + trendsq + prec + precsq, data = dat = "ffips")
 summary(mod5)
+
 
